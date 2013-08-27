05d0d5736dd73f62b3442db3435aeeae909e6126
Jonathan Tsang <chewymeister88@googlemail.com>
2013-08-27 12:29:36 +0100

    Working rspec test with array of sample numbers

--- fizztest.rb
+++ fizztest.rb
@@ -1,17 +1,16 @@
 require "./fizzbuzz"
 
-arr = [2, 5, 19, 73, 20, 10]
+arr = [2, 6, 19, 73, 20, 10]
 
 describe FizzBuzz do
 	before :all do
 		@fizzbuzz = FizzBuzz.new
 	end
-	describe "#fizzbuzz" do
-		it "should say fizz when its 3 or 33" do
-			arr.each {|x|
-				puts
-			}
-		@fizzbuzz.fizzbuzz(x).should eql "Fizz"
+	arr.each do |x|
+		describe "#fizzbuzz" do
+			it "should say fizz when its 3 or 33" do
+			@fizzbuzz.fizzbuzz(x).should eql "Fizz"
+			end
 		end
 	end
 end
